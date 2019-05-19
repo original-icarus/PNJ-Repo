543dc746d2b3753b0e569b30e660cab275262d14
J.Kayser <j.kayser@tu-braunschweig.de>
2019-05-19 15:02:15 +0200

    Zeit - Auslese - Anfang

---
+++ Zeitauslese/Zeitauslese.ino
@@ -0,0 +1,10 @@
+// hier kommt der Code um die Zeit auszulesen rein
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
