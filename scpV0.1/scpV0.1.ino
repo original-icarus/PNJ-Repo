2821b5de796648f38fe721efe65d4838dbb62ee7
J.Kayser <j.kayser@tu-braunschweig.de>
2019-05-20 14:06:26 +0200

    Erstes Auslesen + Displayausgabe
    Erstes auslesen der Temperatur und Ausgabe über LCD des Arduino. Platzhalter für den Feuchtigkeitssensor.

--- scpV0.1/scpV0.1.ino
+++ scpV0.1/scpV0.1.ino
@@ -13,17 +13,27 @@
   // Funktion_8: Manuelle Steuerung
   // Funktion_9: Menü
 
+// Bibliotheken Laden
+#include <LiquidCrystal.h> //LCD-Bibliothek laden
+
+
 // Variablen Defintion:
 
 int TMP36 = A0; //Der Sensor soll am analogen Pin A0 angeschlossen werden. Wir nennen den Pin ab jetzt "TMP36"
 int sensorwert;
 int temperatur = 0; //Unter der Variablen "temperatur" wird später der Temperaturwert abgespeichert.
-int t=500; //Der Wert für „t“ gibt im Code die zeitlichen Abstände zwischen den einzelnen Messungen vor.
+int t=5000; //Der Wert für „t“ gibt im Code die zeitlichen Abstände zwischen den einzelnen Messungen vor.
+
+// LCD 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //In dieser Zeile wird festgelegt, welche Pins des Mikrocontrollerboards für das LCD verwendet wird (Am besten erstmal nicht verändern).
 
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600); //Im Setup beginnt die serielle Kommunikation, damit die Temperatur an den serial monitor übertragen wird. Über die serielle Kommunikation sendet das Board die Messwerte an den Computer. In der Arduino-Software kann man unter „Werkzeuge“ den „Seriellen Monitor“ starten um die Messwerte zu sehen.
+
+// LCD 
+lcd.begin(16, 2); //Im Setup wird angegeben, wie viele Zeichen und Zeilen verwendet werden. Hier: 16 Zeichen in 2 Zeilen.
 }
 
 void loop() {
@@ -33,6 +43,16 @@ temperatur= map(sensorwert, 0, 410, -50, 150); //Umwandeln des Sensorwertes mit 
 delay(t); // Nach jeder Messung ist je eine kleine Pause mit der Dauer „t“ in Millisekunden.
 Serial.print(temperatur); //Nun wird der Wert „temperatur“ über die serielle Kommunikation an den PC gesendet. Durch öffnen des seriellen Monitors in der Arduino-Software kann die Temperatur abgelesen werden.
 Serial.println(" Grad Celsius"); // Im seriellen Monitor wird hinter der Temperatur die Einheit eingeblendet.
-}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//LCD Anzeige
+lcd.setCursor(0, 0); //Startposition der Darstellung auf dem LCD festlegen. lcd.setCursor(0,0) bedeutet: Erstes Zeichen in der ersten Zeile.
+lcd.print("Feuchtigkeit: XX%"); //Dort soll der Text „www.funduino.de“ erscheinen. Der Befehl lcd.setCursor ist dem Mikrocontrollerboard durch das Aufrufen der Bibliothek bekannt.
+lcd.setCursor(0, 1); // lcd.setCursor(0,1) bedeutet: Erstes Zeichen in der zweiten Zeile.
+lcd.print("Temperatur:  ");
+lcd.print(temperatur);
+lcd.print("C"); 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
